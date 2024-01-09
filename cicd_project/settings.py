11a8f22a3fa7b3ef7d5c52a2d6e35e5df8b98435
dev5 <kuldeepdevda319@gmail.com>
2024-01-09 14:02:15 +0530

    fix issue of pytest changes6

--- cicd_project/settings.py
+++ cicd_project/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'cicd_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pipline',
-        'USER': 'postgres',
-        'PASSWORD': 'psql',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME', 'pipline'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'psql'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }   
 
---
+++ pipe/test/test_views.py
@@ -0,0 +1,17 @@
+from django.test import Client
+import pytest
+
+
+def test_home_view():
+    client = Client()
+    response = client.get('/home/')
+    assert response.status_code == 200
+    assert b'Welcome to the CICD Pipeline app!' in response.content
+
+@pytest.mark.django_db
+def test_dashboard_view():
+    client = Client()
+    response = client.get('/dashboard/')
+    assert response.status_code == 200
+    assert b'Hello from the Kuldeep Devda Website!' in response.content
+
